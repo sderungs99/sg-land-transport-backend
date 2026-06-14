e106fcb4bcf24194d44ae6b13f868f40d7dcfa87
Sascha Derungs <sascha.derungs@gmail.com>
2026-06-14 14:47:59 +0800

    spring boot initializer project

---
+++ src/main/java/com/bytecrumbs/sg_land_transport_backend/SgLandTransportBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bytecrumbs.sg_land_transport_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgLandTransportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgLandTransportBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytecrumbs/sg_land_transport_backend/SgLandTransportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytecrumbs.sg_land_transport_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgLandTransportBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
